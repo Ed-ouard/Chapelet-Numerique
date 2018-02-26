3801098e4fe325282b0828a8f6b23917b8931b38
Edouard <a93160@gmail.com>
2018-02-26 18:52:04 +0100

    Button + Buzzer + sequence

--- chapelet-numerique.ino
+++ chapelet-numerique.ino
@@ -1,7 +1,13 @@
 int SER_Pin = 7;   //pin 14 on the 75HC595
 int RCLK_Pin = 8;  //pin 12 on the 75HC595
 int SRCLK_Pin = 9; //pin 11 on the 75HC595
- 
+int LED1 = 12; //pin 12
+int LED2 = 13; //pin 13
+
+const int buttonPin = 2; 
+const int vibreur = 6; 
+int buttonState = 0;
+int state = 0;
 //How many of the shift registers - change this
 #define number_of_74hc595s 1 
  
@@ -16,6 +22,10 @@ void setup(){
   pinMode(SER_Pin, OUTPUT);
   pinMode(RCLK_Pin, OUTPUT);
   pinMode(SRCLK_Pin, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(vibreur, OUTPUT);
+  pinMode(buttonPin, INPUT);
  
   //reset all register pins
   clearRegisters();
@@ -24,13 +34,13 @@ void setup(){
  
   digitalWrite(RCLK_Pin, LOW);
  setRegisterPin(0, LOW);
-  setRegisterPin(1, HIGH);
-  setRegisterPin(2, HIGH);
+  setRegisterPin(1, LOW);
+  setRegisterPin(2, LOW);
   setRegisterPin(3, LOW);
   setRegisterPin(4, LOW);
   setRegisterPin(5, LOW);
- setRegisterPin(6, HIGH);
- setRegisterPin(7, HIGH);
+ setRegisterPin(6, LOW);
+ setRegisterPin(7, LOW);
   digitalWrite(RCLK_Pin, HIGH);
   digitalWrite(RCLK_Pin, LOW);
 }               
@@ -73,10 +83,71 @@ void setRegisterPin(int index, int value){
     digitalWrite(SER_Pin, value);
     digitalWrite(SRCLK_Pin, HIGH);
 }
+
+void setActivePin(int value){
+  digitalWrite(RCLK_Pin, LOW);
+  digitalWrite(RCLK_Pin, HIGH);
+  for(int i = numOfRegisterPins - 1; i >=  0; i--){
+    digitalWrite(SRCLK_Pin, LOW);
+    if(i == value) {
+      digitalWrite(SER_Pin, HIGH);
+    }
+    else digitalWrite(SER_Pin, LOW);
+    digitalWrite(SRCLK_Pin, HIGH);
+  }
+  digitalWrite(RCLK_Pin, LOW);
+}
+ 
  
 void loop(){
  
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+ 
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    Serial.print("Boutton !!!!");
+    next();
+    //vibrate();
+  } else {
+    // turn LED off:
+  }
  
  // writeRegisters();  //MUST BE CALLED TO DISPLAY CHANGES
   //Only call once after the values are set how you need.
 }
+
+void next() {
+  if(state==4) {
+    //state++;
+  }
+  switch(state) {
+    case 0: 
+    setActivePin(-1);
+    digitalWrite(LED1, HIGH); state++; break;
+    case 1: digitalWrite(LED1, LOW); digitalWrite(LED2, HIGH); state++; break;
+    case 2: digitalWrite(LED1, LOW); digitalWrite(LED2, LOW);
+    setActivePin(state-2);
+    state++; 
+    break;
+    default:
+    Serial.println("default");
+    Serial.println(state-2);
+    setActivePin(state-2);
+    state++;
+  if(state==11) {
+    vibrate();
+    state=0;}       
+  }
+  
+  digitalWrite(RCLK_Pin, LOW);
+    delay(200);
+}
+void vibrate() {
+    digitalWrite(vibreur, HIGH);
+    delay(1000);
+    digitalWrite(vibreur, LOW);
+}
+
+
